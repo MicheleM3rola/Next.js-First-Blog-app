7e47d0a5ae536da156d63eaa9d6e7b43bdc98132
Mike <mike88.merola@gmail.com>
2021-02-14 10:35:47 +0800

    fixed spelling of layout component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
